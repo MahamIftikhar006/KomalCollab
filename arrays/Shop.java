87b53954a8fbda2fcbd59467317002ed8ae82e83
MahamIftikhar006 <mahamiffi1998@gmail.com>
2024-10-03 13:30:03 +0500

    Add files via upload

---
+++ arrays/Shop.java
@@ -0,0 +1,24 @@
+public class Shop{
+
+private String name;
+private String address;
+private String phoneno;
+private Product product;
+
+public void setName(String na){
+this.name=na;
+}
+
+public void setAddress(String ad){
+this.address=ad;
+}
+
+public void setPhoneno(){
+
+
+}
+
+
+
+
+}
